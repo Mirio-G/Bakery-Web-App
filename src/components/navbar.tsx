1b9c958cbe74f4f7c08d299eedd6d06e93195012
Jerry Nguyen <jerrynguyen927@gmail.com>
2024-02-22 09:02:15 -0600

    centering footer

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import Container from 'react'
+import '../style/index.css'
 
 function Navbar() {
     return (
         <div className="container">
             <nav className="navbar fixed-bottom navbar-light bg-light">
-            <a className="nav-item nav-link" href="#"><h2 id = "makeLeft"><i><b>Menu</b></i></h2></a>
+            <a className="nav-item nav-link" href="#">
+                <h2 id = "makeLeft"><i><b>Menu</b></i></h2>
+                </a>
             <a className="navbar-brand" href="#">
                 <img src="./src/assets/toplogo.png" alt="logo" id="navlogo"></img>
             </a>
-            <a className="nav-item nav-link" href="#"><h2 id = "makeRight"><i><b>Checkout</b></i></h2></a>
+            <a className="nav-item nav-link" href="#">
+                <h2 id = "makeRight"><i><b>Checkout</b></i></h2></a>
             </nav>
         </div>
     );
