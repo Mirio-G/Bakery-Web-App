3d79d559955208f8af28a4b10ff5a84f3d173ff3
Tri Bui <tbui931958@gmail.com>
2024-02-28 11:11:30 -0600

    remove socket

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import socketIO from 'socket.io-client';
 
 import App from './Home.tsx';
 import ChatPage from "./components/chat/ChatPage.tsx";
@@ -15,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App socket={socket} />}></Route>
+        <Route path="/" element={<App />}></Route>
         <Route path="/Checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
