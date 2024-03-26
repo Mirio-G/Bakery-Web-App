7085144855e36d07f75600f7002e2bbcc22bc925
Jerry Nguyen <jerrynguyen927@gmail.com>
2024-03-26 08:37:40 -0500

    created route for menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useMemo, useContext } from "react";
+
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+
+ // We import all the components we need in our app
+import Navbar from "./navbar.tsx";
+
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+// styling
+import '../style/App.css'
+import '../style/card.css'
+
+// bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Menu(){
+
+}
+export default Menu
