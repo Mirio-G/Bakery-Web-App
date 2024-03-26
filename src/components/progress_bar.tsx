8694bd47825ec6626927005d3674f2676803784e
Jerry Nguyen <jerrynguyen927@gmail.com>
2024-03-26 07:58:12 -0500

    progress bar progress

--- src/components/progress_bar.tsx
+++ src/components/progress_bar.tsx
@@ -2,7 +2,7 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 import '../style/index.css'
 
 function PBar() {
-  const now = 20;
+  const now = 50;
   return  <ProgressBar now={now} label={`${now}%`} />;
 }
 
