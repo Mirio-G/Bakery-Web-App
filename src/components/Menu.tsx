9f1e5bf5eadec01016f59f4312180d151ef92041
Luis Garibay <garib409919@aisd.net>
2024-03-28 09:03:25 -0500

    add data to cards in menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useMemo, useContext } from "react";
+
+import axios from 'axios';
 
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
@@ -17,23 +19,52 @@ import Container from 'react-bootstrap/Container';
 import Row, { RowProps } from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+interface Menu {
+    breadItems: any[];
+    drinkItems: any[];
+  }
 function Menu(){
+
+    const [breadItems, setBreadItems] = useState([]);
+    const [drinkItems, setDrinkItems] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const breadsResponse = await axios.get('http://localhost:5000/route/breads');
+                setBreadItems(breadsResponse.data);
+
+                const drinksResponse = await axios.get('http://localhost:5000/route/drinks');
+                setDrinkItems(drinksResponse.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return(
         <>
     <Navbar/>
-    <Container class = "menu_container">
+    <Container className = "menu_container">
         <Row>
             <Col>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                {drinkItems.map((drink, index) => {
+                    return (
+                    <div key={index}>
+                    <Card type={drink.type} img={drink.img} topping={drink.topping} description={drink.description} price={drink.price} data={drink}/>
+                    </div> )})
+                }
             </Col>
+
             <Col>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                {breadItems.map((bread, index) => {
+                    return (
+                    <div key={index}>
+                    <Card type={bread.type} img={bread.img} topping={bread.topping} description={bread.description} price={bread.price} data={bread}/>
+                    </div> )})
+                }
             </Col>
         </Row>
     </Container>
